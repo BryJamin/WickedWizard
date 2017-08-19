f2690f81cd8894350963ff3e7fd1e8ca56b6d429
ae164 <benjaminabryante@hotmail.co.uk>
2017-08-19 11:40:48 +0100

    Made changes to some songs

--- core/src/com/byrjamin/wickedwizard/assets/MusicStrings.java
+++ core/src/com/byrjamin/wickedwizard/assets/MusicStrings.java
@@ -8,13 +8,13 @@ public class MusicStrings {
 
     public static final Mix BG_MAIN_MENU = new Mix.MixMaker("audio/music/Contemplation-03.ogg").build();
 
-    public static final Mix BG_LEVEL_ONE = new Mix.MixMaker("audio/music/Uno.ogg").build();
+    public static final Mix BG_LEVEL_ONE = new Mix.MixMaker("audio/music/Field.ogg").build();
 
-    public static final Mix BG_LEVEL_TWO = new Mix.MixMaker("audio/music/L_March.ogg").build();
+    public static final Mix BG_LEVEL_TWO = new Mix.MixMaker("audio/music/SecondStep.ogg").build();
 
-    public static final Mix BG_LEVEL_THREE = new Mix.MixMaker("audio/music/Shanty.ogg").build();
+    public static final Mix BG_LEVEL_THREE = new Mix.MixMaker("audio/music/SittingAloneInMyRoomAndThisIsAllICouldComeUpWith-02.ogg").build();
 
-    public static final Mix BG_LEVEL_FOUR = new Mix.MixMaker("audio/music/Underground.ogg").build();
+    public static final Mix BG_LEVEL_FOUR = new Mix.MixMaker("audio/music/Mire.ogg").build();
 
     public static final Mix BG_LEVEL_FIVE = new Mix.MixMaker("audio/music/Forward-01.ogg").build();
 
