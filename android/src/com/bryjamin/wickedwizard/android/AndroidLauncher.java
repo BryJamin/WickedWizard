cea7d63d964c31c58ec39ce114791b1729768d7b
ae164 <benjaminabryante@hotmail.co.uk>
2017-09-05 19:38:47 +0100

    Added Sticky immersive mode

--- android/src/com/bryjamin/wickedwizard/android/AndroidLauncher.java
+++ android/src/com/bryjamin/wickedwizard/android/AndroidLauncher.java
@@ -1,6 +1,8 @@
 package com.bryjamin.wickedwizard.android;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -12,26 +14,24 @@ public class AndroidLauncher extends AndroidApplication {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new MainGame(), config);
-
+/*
 		config.hideStatusBar = true;
-		config.useImmersiveMode = true;
+		config.useImmersiveMode = true;*/
 	}
 
 	@Override
 	public void onWindowFocusChanged(boolean hasFocus) {
 		super.onWindowFocusChanged(hasFocus);
-/*		if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+
+		if (hasFocus && Build.VERSION.SDK_INT >= 19) {
 			getWindow().getDecorView().setSystemUiVisibility(
 					View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 							| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 							| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 							| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 							| View.SYSTEM_UI_FLAG_FULLSCREEN
-							| View.SYSTEM_UI_FLAG_IMMERSIVE
 							| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-		}*/
-
-
+		}
 
 	}
 
